145517437198d1071e632109e1be7c86ce00d457
justforuse <allenforuse@gmail.com>
2022-02-15 22:57:27 +0800

    dev: support web extension

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,22 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-// import { toCamelCase, toConstant, toPascal, toPhp } from './function'
-const util = require("./function");
-const vscode = require("vscode");
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
+export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "var-style" is now active!');
+  console.log(
+    'Congratulations, your extension "var-style" is now active in the web extension host!'
+  );
 
   // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
+  // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable1 = vscode.commands.registerCommand(
     "extension.camelStyle",
@@ -107,12 +103,5 @@ function activate(context) {
   context.subscriptions.push(disposable5);
 }
 
-exports.activate = activate;
-
 // this method is called when your extension is deactivated
-function deactivate() {}
-
-module.exports = {
-  activate,
-  deactivate,
-};
+export function deactivate() {}
